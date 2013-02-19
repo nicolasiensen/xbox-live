eeb2d02ce2e247cc74243772e15c8e1aa09f817c
Nícolas Iensen <nicolas@engage.is>
2013-02-19 09:04:56 -0300

    XboxLive.achievements and XboxLive.friends methods

--- lib/xboxlive-wrapper.rb
+++ lib/xboxlive-wrapper.rb
@@ -9,4 +9,12 @@ module XboxLive
   def self.games gamertag
     MultiJson.load(Faraday.get("http://www.xboxleaders.com/api/games.json?gamertag=#{gamertag}").body)
   end
+  
+  def self.achievements gamertag, title_id
+    MultiJson.load(Faraday.get("http://www.xboxleaders.com/api/achievements.json?gamertag=#{gamertag}&titleid=#{title_id}").body)
+  end
+  
+  def self.friends gamertag
+    MultiJson.load(Faraday.get("http://www.xboxleaders.com/api/friends.json?gamertag=#{gamertag}").body)
+  end
 end
--- spec/xbox_live_spec.rb
+++ spec/xbox_live_spec.rb
@@ -26,4 +26,30 @@ describe XboxLive do
       end
     end
   end
+
+  describe ".achievements" do
+    context "when the gamertag exists and the title id too" do
+      before do
+        Faraday.stub("get").
+          with('http://www.xboxleaders.com/api/achievements.json?gamertag=nicolasjensen&titleid=1161890128').
+          and_return(double(:body => File.open("spec/support/achievements.json")))
+      end
+      it "should return a hash with the achievements data" do
+        XboxLive.achievements("nicolasjensen", "1161890128")["Data"]["Achievements"][0]["Title"].should be_== "Scrap Metal"
+      end
+    end
+  end
+
+  describe ".friends" do
+    context "when the gamertag exists" do
+      before do
+        Faraday.stub("get").
+          with('http://www.xboxleaders.com/api/friends.json?gamertag=nicolasjensen').
+          and_return(double(:body => File.open("spec/support/friends.json")))
+      end
+      it "should return a hash with the friends data" do
+        XboxLive.friends("nicolasjensen")["Data"]["Friends"][0]["Gamertag"].should be_== "cccav"
+      end
+    end
+  end
 end
